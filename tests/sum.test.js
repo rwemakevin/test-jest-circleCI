951d0e87932b446d02a78d5da783da0b508eace5
KevinRwema <kevinrwema@gmail.com>
2024-04-03 19:31:52 +0200

    Add One more test to sum test

--- tests/sum.test.js
+++ tests/sum.test.js
@@ -3,3 +3,7 @@ import { sum } from "../src/helper";
 test("Add 2 to 5 to equal 7", () => {
   expect(sum(2, 5)).toBe(7);
 });
+
+test("Add 1 to 5 to equal 6", () => {
+  expect(sum(1, 5)).toBe(6);
+});
