f60bae8031bf7c15c108da5e5186edffa1a4abf5
KevinRwema <kevinrwema@gmail.com>
2024-04-05 14:59:31 +0200

    ch: arguments test

--- tests/sum.test.js
+++ tests/sum.test.js
@@ -4,6 +4,6 @@ test("Add 2 to 5 to equal 7", () => {
   expect(sum(2, 5)).toBe(7);
 });
 
-test("Add 1 to 5 to equal 6", () => {
-  expect(sum(1, 5)).toBe(6);
+test("Add 1 to 7 to equal 8", () => {
+  expect(sum(1, 7)).toBe(8);
 });
