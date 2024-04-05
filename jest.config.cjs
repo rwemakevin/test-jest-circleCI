42c6c74ec44af8a0fd073f12d064ed830abb9418
KevinRwema <kevinrwema@gmail.com>
2024-04-05 15:44:49 +0200

    Test CodeClimate Integration 1

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,18 @@
 /** @type {import('jest').Config} */
 const config = {
   verbose: true,
+  coverageDirectory: "coverage", // Specify the directory where coverage reports will be stored
+  collectCoverage: true, // Enable collecting coverage
+  collectCoverageFrom: ["src/**/*.js"], // Specify the files for which coverage should be collected
+  coverageReporters: ["lcov", "text-summary"], // Specify the coverage report formats
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
 
 module.exports = config;
